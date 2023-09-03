aeaae27f7545aa20a1bc3a5941c3e92ef773c5cc
guicvsub <rm552321@fiap.com.br>
2023-09-02 21:31:43 -0300

    erro de digitação corrigido

--- 10.py
+++ 10.py
@@ -1,6 +1,6 @@
 l1=int(input("digite o lado 1"))
-l2=int(input("digite o lado 1"))
-l3=int(input("digite o lado 1"))
+l2=int(input("digite o lado 2"))
+l3=int(input("digite o lado 3"))
 if l1 == l2 == l3: 
     print("esse triangulo e equilatero")
 elif l1==l2 or l1==l3 or l2==l3:

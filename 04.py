2313b59c759de847123a8ab7f5eb7ded4b877155
guicvsub <143191040+guicvsub@users.noreply.github.com>
2023-09-06 09:00:54 -0300

    Update 04.py

--- 04.py
+++ 04.py
@@ -1,5 +1,5 @@
 quant=int(input("digite a quantidade"))
-if quant==11:
+if quant< 12:
     print(f"valor total da compra e R%{(0.30*quant)}")
 else:
     print(f"o valor total da compra e  R${(0.25*quant)}")

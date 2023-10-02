59d379f640ecc6ebe93e354d3c52ce45e271e948
guicvsub <rm552321@fiap.com.br>
2023-10-01 23:46:57 -0300

    esse e o jogo de adi

--- jogo de adivinhacao.py
+++ jogo de adivinhacao.py
@@ -3,12 +3,16 @@ print(" jogo de adivinhação ")
 print("*********************")
 numero_secreto = 42
 chute = int(input("digite um numero "))
-if chute==numero_secreto:
-    print ("acertou o número secreto era",numero_secreto)
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+acertou = chute ==numero_secreto
+if (acertou):
+
+ print ("acertou o número secreto era",numero_secreto)
 else :
-    print("voce errou")
-    if numero_secreto > chute:
-                print("digite um numero menor")
-    else:
-        print("digite um numero maior")
+ print("voce errou") 
+if (menor):
+ print("digite um numero menor")
+else:
+ print("digite um numero maior")
                         

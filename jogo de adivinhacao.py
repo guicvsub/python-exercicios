1f8e3a64feebe3e20e31d7e8f50db9d503ad01c8
guicvsub <rm552321@fiap.com.br>
2023-10-02 09:38:39 -0300

    adicionando tentativas

--- jogo de adivinhacao.py
+++ jogo de adivinhacao.py
@@ -1,18 +1,22 @@
 print("*********************")
 print(" jogo de adivinhação ")
 print("*********************")
+contador = 5
 numero_secreto = 42
 chute = int(input("digite um numero "))
 maior = chute > numero_secreto
 menor = chute < numero_secreto
 acertou = chute ==numero_secreto
-if (acertou):
+while  (chute!=acertou and contador > 0):
 
  print ("acertou o número secreto era",numero_secreto)
-else :
- print("voce errou") 
-if (menor):
- print("digite um numero menor")
-else:
- print("digite um numero maior")
-                        
+ if(acertou):
+    print("voce errou") 
+ if (menor):
+  print("digite um numero menor")
+  contador-=1 
+  chute = int(input("digite um numero "))
+ else:
+    print("digite um numero maior")
+    contador-=1 
+    chute = int(input("digite um numero "))

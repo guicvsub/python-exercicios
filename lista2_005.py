a67d2f48577d3941d0d22057e0db72bc346961bf
guicvsub <rm552321@fiap.com.br>
2024-02-17 17:13:34 -0300

    fazendo atividades

--- lista2_005.py
+++ lista2_005.py
@@ -1,12 +1,76 @@
-n1=int(input("dgite un numero"))
-n2=int(input("dgite un numero"))
-n3=int(input("dgite un numero"))
-maior = n1
-medio= n2
-menor = n3
-if maior < medio and maior< menor:
-    menor= n1
-    medio=n3
-    menor=n2
-    if medio<
-print(maior,medio,menor)
+'''# -*- coding: utf-8 -*-
+
+'''
+
+'''
+A = float(input())
+B = float(input())
+PESO_A = 3.5
+PESO_B = 7.5
+SOMA_PESOS = PESO_A + PESO_B
+MEDIA = ((A*PESO_A)+(B*PESO_B))/SOMA_PESOS
+print("MEDIA = {:.5f}".format(MEDIA))
+'''
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+'''A = float(input())
+B = float(input())
+C = float(input())
+SOMA_PESSOS = 2+3+5
+MEDIA = ((A*2)+(B*3)+(C*5))/SOMA_PESSOS
+print("MEDIA ={:.1f}".format(MEDIA))'''
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+'''A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+DIFERENCA=((A*B)-(C*D))
+print("DIFERENÇA =", DIFERENCA)'''
+
+'''numero = int(input())
+horasTrabalhadas = int(input())
+valorHora = float(input())
+salario = horasTrabalhadas*valorHora
+print("NUMBER=", numero)
+print("SALARY = U$ {:.2f}".format(salario))
+'''
+
+'''numero = int(input())
+horasTrabalhadas = int(input())
+valorHora = float(input())
+salario = horasTrabalhadas*valorHora
+print("NUMBER=", numero)
+print("SALARY = U$ {:.2f}".format(salario))'''
+
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+'''nome = input()
+salario= float(input())
+vendas = float(input())
+comissao = (15/100)
+total =salario +(comissao*vendas)
+print("TOTAL ={:.2f}".format(total))'''
+# Definindo uma string
+frase = "Olá, como vai você?"
+
+# Dividindo a string em uma lista de palavras
+palavras = frase.split()
+
+print(palavras)

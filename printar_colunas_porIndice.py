6176bb681f2c5d417c575db3e8f64749c52ba479
guicvsub <rm552321@fiap.com.br>
2024-02-21 16:37:45 -0300

    fazendo atividades

---
+++ printar_colunas_porIndice.py
@@ -0,0 +1,20 @@
+def acha_maior(lista):
+    maior=lista[0]
+    for i in range(len(lista)):
+        if lista[i] > maior:
+            maior=lista[i]
+    return maior
+def acha_indice_maior(lista):
+    indice = 0
+    maior= lista[0]
+    for i in range(len(lista)):
+        if lista[i]>maior:
+            maior= lista[i]
+            indice=i
+    return indice
+
+caros =["uno","gol","celtinha"]
+precos =[10,20,1000]
+caroMaisCaro= caros[acha_indice_maior(precos)]
+precosMaior= acha_maior(precos)
+print(f"esse e o carro mais carro e o {caroMaisCaro} e o preco e {precosMaior} ")

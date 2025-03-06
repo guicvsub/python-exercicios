560539c439b92c7af4cac204752d4b19112d64b0
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-06 09:43:35 -0300

    anotaçoes-cp1-python-segundo-semestre-2024-att

--- busca_binaria.py
+++ busca_binaria.py
@@ -10,10 +10,23 @@ def busca_binaria(lista , item):
             alto= meio - 1
         else:
             baixio = meio + 1
-    return None
+    return print(f"o chute {item} nao está na lista" )
 
 minha_lista= [1,3,5,7,9]
-a = busca_binaria(minha_lista,9)
+lista_ordenada = sorted(minha_lista)
+a = busca_binaria(lista_ordenada,18)
+
+
 print(a)
 
 
+
+
+lista_dois=[("maca", 3),("pitaia", 14),("abacate", 2),("banana", 5)]
+tupla_ordenada = sorted(lista_dois, key=lambda x : x[1])
+print(tupla_ordenada)
+
+
+lista_dois=[("maca", 3,"c"),("pitaia", 14,"d"),("abacate", 2,"e"),("banana", 5,"f")]
+tupla_ordenada2 = sorted(lista_dois, key=lambda y :(y[1], y[2]))
+print(tupla_ordenada2)
